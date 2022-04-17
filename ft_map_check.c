c79ba3b7e878e464d5bb39aad2ec24098e695777
nvj1d <najid99m@gmail.com>
2022-04-17 23:43:45 +0000

    ft_check_map

--- ft_map_check.c
+++ ft_map_check.c
@@ -6,12 +6,65 @@
 /*   By: mnajid <mnajid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 22:37:33 by mnajid            #+#    #+#             */
-/*   Updated: 2022/04/17 22:51:38 by mnajid           ###   ########.fr       */
+/*   Updated: 2022/04/17 23:43:18 by mnajid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-ft_map_chars_num()
+
+void	ft_check_walls(t_map *map)
+{
+	int		i;
+	int		j;
+	int		rows;
+	int		columns;
+	char	**map_arr;
+
+	rows = map->height - 1;
+	columns = map->width - 1;
+	map_arr = map->map_array;
+	i = -1;
+	while (++i <= rows)
+	{
+		j = -1;
+		while (++j <= columns)
+		{
+			if (map_arr[0][j] != '1' || map_arr[rows][j] != '1'
+				|| map_arr[i][0] != '1' || map_arr[i][columns] != '1')
+				ft_map_errors(map, 3);
+		}
+	}
+}
+
+void	ft_check_rect(t_map *map, char c, int i, int j)
+{
+	int		i;
+	char	**map_arr;
+
+	map_arr = map->map_array;
+	i = -1;
+	while (map_arr[++i])
+	{
+		if ((int)ft_strlen(map_arr[i] - 1) != map->width)
+			ft_map_errors();
+	}
+}
+
+
+void	ft_map_chars_num(t_map *map, char c, int i, int j)
+{
+	if(c == '0' || c == '1')
+		return;
+	else if (c == 'E')
+		map->item++;
+	else if (c == 'P')
+	{
+		map->startp++;
+		map->x = j;
+		map->y = i;
+	}
+}
+
 //to check if the map is valid or not!
 void	ft_map_check(t_map *tmap)
 {
